fa75b03b2dfe2ad7bc15ca650268874d060df854
Sivashree.S <Sivashree.S@EXIND3010-132.allfon.in>
2022-03-21 12:54:47 +0530

    project

--- jenkins-git-maven-docker-build/src/main/java/com/example/demo/TeacherController.java
+++ jenkins-git-maven-docker-build/src/main/java/com/example/demo/TeacherController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,15 @@ public class TeacherController {
 	public String getTeacher() {
 		List<String> list =Arrays.asList("ram","sam","stella");
 		return list.toString();
+	}
+	@GetMapping(path ="/teachers/{dept}")
+	public String getTeachersByDept(@PathVariable("dept") String dept) {
+		
+		List<String> list = Arrays.asList("ram","sam","stella");
+		if(dept.equals("ece")) {
+			list = Arrays.asList("Shankar","Salim","Simon");
+		}
+		return list.toString();
+		
 	}
 }
